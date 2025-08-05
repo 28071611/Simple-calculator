36f76d201c8eb706d6e662b5e30c7dabaad007fa
HARISH R <harishpblr2007@gmail.com>
2025-08-05 14:59:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let display = document.getElementById('display');
+
+// Append value to the display
+function appendValue(value) {
+    display.value += value;
+}
+
+// Clear the display
+function clearDisplay() {
+    display.value = '';
+}
+
+// Calculate the result
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
